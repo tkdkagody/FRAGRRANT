ca6b6a73d834a3a8868d80116b38905ac0df6e6b
tkdkagody <tkdkagody@gmail.com>
2021-11-20 01:06:34 +0900

    Edit: error msg

--- client/src/component/Signup/index.tsx
+++ client/src/component/Signup/index.tsx
@@ -100,7 +100,7 @@ function Signup ({signupModal, setSignupModal} : MainProps) {
                             <Input value={passwordCheck} onChange={onChangePasswordCheck}/>
                         </div>
                         {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-                        {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+                        {/* {!nickname && <Error>닉네임을 입력해주세요.</Error>} */}
                         {signUpError && <Error>{signUpError}</Error>}
                         {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
                     </Label>
